97f1c2dba2fc72925807f275e239085ce75aa93c
River Bryant <river.bryant@ibethel.org>
2020-02-01 19:49:12 -0800

    build auto deploy and update text

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '34.207.167.13',
+      host: '54.89.175.159',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
